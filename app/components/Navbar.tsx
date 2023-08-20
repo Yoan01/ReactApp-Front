d53efbc4752230bf6a2ae9ee58d2a2df20fc3eb0
Yoan Ranchon <yoan.ranchon@gmail.com>
2023-08-20 11:01:22 +0200

    navbar finit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,110 @@
+import Link from "next/link"
+import PersonIcon from "@mui/icons-material/Person"
+import { useState } from "react"
+import { Drawer } from "@mui/material"
+import AdbIcon from "@mui/icons-material/Adb"
+import AppleIcon from "@mui/icons-material/Apple"
+import Image from "next/image"
+
 export const Navbar = ({ bgColor }: { bgColor: string }) => {
+  const [open, setOpen] = useState(false)
+
+  const toggleDrawer = (isOpen: any) => {
+    setOpen(isOpen)
+  }
+
   return (
-    <nav className={`p-8 ${bgColor}`}>
-      <div className="flex gap-7 items-center">
-        <div className="w-5 h-[14px] cursor-pointer relative">
-          <span className="bg-black h-[2px] absolute w-full left-0 rounded-xl top-0"></span>
-          <span className="bg-black h-[2px]  absolute w-full left-0 rounded-xl top-[6px]"></span>
-          <span className="bg-black h-[2px]  absolute w-full left-0 rounded-xl top-3"></span>
+    <>
+      <nav
+        className={`px-10 py-6 ${bgColor} fixed w-full flex items-center justify-between`}
+      >
+        <div className='flex gap-8 items-center'>
+          <div
+            className='w-5 h-[14px] cursor-pointer relative'
+            onClick={() => toggleDrawer(true)}
+          >
+            <span className='bg-black h-[2px] absolute w-full left-0 rounded-xl top-0'></span>
+            <span className='bg-black h-[2px]  absolute w-full left-0 rounded-xl top-[5.8999px]'></span>
+            <span className='bg-black h-[2px]  absolute w-full left-0 rounded-xl top-3'></span>
+          </div>
+          <span className='text-3xl'>
+            Uber <span className='font-bold'>Eats</span>
+          </span>
         </div>
-        <span className="text-2xl">
-          Uber <span className="font-bold">Eats</span>
-        </span>
-      </div>
-      <div></div>
-    </nav>
+        <div className='flex gap-4'>
+          <Link
+            href={"/login"}
+            className='bg-white rounded-full font-bold py-3 px-4 hover:bg-[#cccccc] flex items-center gap-2'
+          >
+            <PersonIcon />
+            <button>Connexion</button>
+          </Link>
+          <Link
+            href={"/sign"}
+            className='bg-black text-white font-bold rounded-full py-3 px-4 hover:bg-[#333333]'
+          >
+            <button>Inscription</button>
+          </Link>
+        </div>
+      </nav>
+      <Drawer anchor='left' open={open} onClose={() => toggleDrawer(false)}>
+        <div className='w-[252px] m-6 flex flex-col justify-between h-full'>
+          <div>
+            <div className='flex flex-col gap-4 items-center justify-center'>
+              <Link
+                href={"/sign"}
+                className='bg-black text-white rounded-lg py-4 w-full hover:bg-[#333333] flex items-center justify-center'
+              >
+                <button>Inscription</button>
+              </Link>
+              <Link
+                href={"/login"}
+                className='bg-[#eeeeee] text-black rounded-lg  py-4 w-full hover:bg-[#e2e2e2] flex items-center justify-center'
+              >
+                <button>Connexion</button>
+              </Link>
+            </div>
+            <div className='flex flex-col pt-5 gap-3'>
+              <Link href={"/pro"} className='text-sm'>
+                Créez un compte professionnel
+              </Link>
+              <Link href={"/restaurant"} className='text-sm'>
+                Ajoutez votre restaurant
+              </Link>
+              <Link href={"/partenaire"} className='text-sm'>
+                Devenez coursier-partenaire
+              </Link>
+            </div>
+          </div>
+          <div className=' flex flex-col'>
+            <div className='flex gap-3 items-center'>
+              <Image
+                alt='Uber&nbsp;Eats'
+                src='https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/31ee382bd0e6ed84.svg'
+                width={56}
+                height={56}
+              />
+              <span className=' text-base text-bold'>
+                Plus de fonctionnalités dans l&apos;application.
+              </span>
+            </div>
+            <div className='flex gap-4 items-center justify-center m-6'>
+              <Link
+                href={"/iphone"}
+                className='bg-[#eeeeee] text-black text-sm rounded-full py-2 px-2 hover:bg-[#e2e2e2] flex items-center justify-center gap-2'
+              >
+                <AppleIcon /> <button>Iphone</button>
+              </Link>
+              <Link
+                href={"/android"}
+                className='bg-[#eeeeee] text-black text-sm rounded-full  py-2 px-2 hover:bg-[#e2e2e2] flex items-center justify-center gap-2'
+              >
+                <AdbIcon /> <button>Android</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Drawer>
+    </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,81 @@
-import { Navbar } from './components/Navbar'
-import styles from './Navbar.module.css'
-import { scroller } from 'react-scroll'
+"use client"
+import { useEffect, useState } from "react"
+import { Navbar } from "./components/Navbar"
+import styles from "./Navbar.module.css"
+import Link from "next/link"
+import TextField from "@mui/material/TextField"
+import MenuItem from "@mui/material/MenuItem"
+import Button from "@mui/material/Button"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import WatchLaterRoundedIcon from "@mui/icons-material/WatchLaterRounded"
 
 const Page = () => {
-  let bgColor: string = 'bg-white'
-  console.log(scroller)
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#000000", // Couleur noire
+      },
+    },
+  })
 
-  // scroller?.addEventListener('scroll', (event) => {
-  //   if (scroller.scrollTop === 0) {
-  //     bgColor = 'bg-transparent'
-  //   }
-  // })
+  const [bgColor, setBgColor] = useState("bg-transparent")
+  const [options, setOptions] = useState([
+    "Livrer maintenant",
+    "Planifier pour plus tard",
+  ])
+  const [optionSelected, setOptionSelected] = useState("Livrer maintenant")
+
+  useEffect(() => {
+    const handleColor = () => {
+      let scrollY = window.scrollY
+
+      if (scrollY != 0) {
+        setBgColor("bg-white transition duration-300")
+      } else {
+        setBgColor("bg-transparent transition duration-300")
+      }
+    }
+
+    window.addEventListener("scroll", handleColor)
+
+    return () => {
+      window.removeEventListener("scroll", handleColor)
+    }
+  }, [])
+
+  const selectOption = (event: any) => {
+    setOptionSelected(event.target.value)
+  }
+
   return (
     <>
-      <div className={`homepage ${styles.homepage} h-screen`}>
-        <Navbar bgColor={bgColor} />
-        <div></div>
+      <Navbar bgColor={bgColor} />
+      <div className={`${styles.homepage} h-screen flex items-center`}>
+        <div>
+          <span>Vos plats préférés, directement à votre porte</span>
+          <div className='flex gap-5'>
+            <TextField
+              id='outlined-select-currency'
+              select
+              value={optionSelected}
+              onChange={selectOption}
+            >
+              <MenuItem value={"Livrer maintenant"}>Livrer maintenant</MenuItem>
+              <MenuItem value={"Planifier pour plus tard"}>
+                Planifier pour plus tard
+              </MenuItem>
+            </TextField>
+            <Link
+              href={"/restaurant"}
+              className='bg-black text-white rounded-lg py-3 px-4 hover:bg-[#333333] flex items-center'
+            >
+              <button>Trouver un restaurant</button>
+            </Link>
+          </div>
+          <span>
+            <Link href={"/sign"}>Sign In</Link> for your recent addresses
+          </span>
+        </div>
       </div>
     </>
   )
